5ebdfa6815a9fed45dba00539902001748a4bc14
Clément Vidal <clementvidalperso@gmail.com>
2015-09-11 16:53:02 +0200

    Work on test conditions

--- demo/script.js
+++ demo/script.js
@@ -1,6 +1,22 @@
 function generateInitialLayout() {
 
+    for( var i=0; i<10; i++ ){
+
+        var div = document.createElement("div");
+        div.className += "node";
+        document.getElementsByClassName('container')[0].appendChild(div);
+    }
 }
 
+generateInitialLayout();
+
 var graph = graphLayout.newGraph();
+
+var nodesDiv = document.getElementsByClassName( 'node' );
+
+for( var i=0;i<nodesDiv.length; i++ ) {
+    
+    graph.addNode( 0, 0, 10, 10 );
+}
+graph.addNode( 0, 0, 10, 10 );
 graph.addEdge( 0, 1);
--- src/graph-layout.js
+++ src/graph-layout.js
@@ -6,20 +6,46 @@
 
         this.adjacencyList = [];
 
+        /**
+         * Add a new node
+         */
         this.addNode = function(x, y, w, h, data) {
-            this.adjacencyList.push(new Array());
-        }
-        this.addEdge = function(n1, n2) {
-            if (n1 === n2 ) {
-                throw "Cannot connect a node to itself";
-            }
-            if (n1 >= this.adjacencyList.length || n2 >= this.adjacencyList.length) {
-                throw "Invalid node index";
-            }
-            this.adjacencyList[n1].push(n2);
-            this.adjacencyList[n1].push(n2);
+
+            var obj = {
+                edgesTo: new Array(),
+                rect: {
+                    x: x,
+                    y: y,
+                    w: w,
+                    h: h
+                },
+                data: data
+            };
+
+            this.adjacencyList.push(obj);
+
+            return obj;
         }
 
+        /**
+         * Add an edge between to nodes
+         */
+        this.addEdge = function(n1, n2) {
+            if (n1 === n2) {
+                throw new Error("Cannot connect a node to itself.");
+            }
+            if (n1 >= this.adjacencyList.length || n2 >= this.adjacencyList.length) {
+                throw new Error("Invalid node index.");
+            }
+
+            if (_.indexOf(this.adjacencyList[n1].edgesTo, n2) == -1) {
+                this.adjacencyList[n1].edgesTo.push(n2);
+            }
+
+            if (_.indexOf(this.adjacencyList[n2].edgesTo, n1) == -1) {
+                this.adjacencyList[n2].edgesTo.push(n1);
+            }
+        }
     }
 
     window.graphLayout = {
